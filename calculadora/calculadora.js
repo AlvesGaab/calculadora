e211746f266a27ca9c243f103b3c4dc6ea349440
Gabriel Alves <eu_alvesgabriel@hotmail.com>
2021-05-24 17:42:37 -0300

    teste

---
+++ calculadora/calculadora.js
@@ -0,0 +1,35 @@
+function insert(num)
+{
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num; 
+}
+
+function clean() 
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function Adição(a , b) 
+{
+     console.log (a + b) 
+} 
+
+
+function Multiplicação(a , b) 
+{ 
+    console.log (a * b) 
+} 
+
+
+function subtração(a , b) 
+{ 
+    console.log (a - b) 
+} 
+
+
+
+function divisão(a , b) 
+{ 
+    console.log (a / b) 
+}
+
