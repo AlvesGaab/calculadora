89f9481ba38ee088de01d8db017fe0278e16df69
Andre Alves <andre.alves@evtit.com>
2021-05-28 21:09:28 -0300

    Adicionando os calculos de numeros simples

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,23 +1,39 @@
+const operacoes = ['+', '-', '*', '/'];
+let num1 = '', num2 = '', operador = '', resultado = 0;
+
 function insert(num)
 {
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num; 
+    const ehOperador = operacoes.includes(num);
+
+    if(num1 && !num2 && !ehOperador)
+        num2 = parseInt(num); 
+
+    if(!num1 && !ehOperador)
+        num1 = parseInt(num);  
+     
+    console.log(num);
+
+    if(ehOperador)
+        operador = num;
+
+    document.getElementById('resultado').innerHTML = num1 +operador+ num2; 
 }
 
 function clean() 
 {
+    num1 = '', num2 = '', resultado = '', operador = '';
     document.getElementById('resultado').innerHTML = "";
 }
 
-function Adição(a , b) 
+function Adicao(a , b) 
 {
-     console.log (a + b) 
+    return a+b;
 } 
 
 
-function Multiplicação(a , b) 
+function Multiplicacao(a , b) 
 { 
-    console.log (a * b) 
+    return a*b;
 } 
 
 
@@ -33,3 +49,20 @@ function divisão(a , b)
     console.log (a / b) 
 }
 
+function calcular(){
+
+    switch (operador) {
+        case '+':
+            resultado = Adicao(num1,num2);
+            break;
+        case '*':
+            resultado = Multiplicacao(num1,num2);
+            break;
+        default:
+            break;
+    }
+
+    
+
+    document.getElementById('resultado').innerHTML = (resultado);
+}
